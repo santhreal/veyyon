9d8665c590994298054d6916f0056d8d77217048
santhreal <64453045+santhreal@users.noreply.github.com>
2026-08-22 21:05:41 -0700

    fix(config): centralize retired search tool migration

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3b188d48.js',
+                path_to_root + 'searchindex-85384dad.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
