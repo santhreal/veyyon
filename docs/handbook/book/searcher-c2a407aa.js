2caae380426c69133374f3a347defb2801e5b203
santhreal <64453045+santhreal@users.noreply.github.com>
2026-08-23 21:03:54 -0700

    docs(handbook): regenerate the settings reference and book after the default change

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4c0dbea2.js',
+                path_to_root + 'searchindex-9a248871.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
