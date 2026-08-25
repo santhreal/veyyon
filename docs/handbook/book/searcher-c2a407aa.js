22738153b49f3c1f61edffaaf5553d59e534c1cb
santhreal <64453045+santhreal@users.noreply.github.com>
2026-08-24 17:05:21 -0700

    chore(docs): rebuild the handbook for the prewalk settings reference

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2c0008ea.js',
+                path_to_root + 'searchindex-037168c5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
