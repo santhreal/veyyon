0443775183f70d8abb9c04797ace27e008b70a83
santhreal <64453045+santhreal@users.noreply.github.com>
2026-08-28 08:27:47 -0700

    docs: rebuild stale handbook book

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-89f274d4.js',
+                path_to_root + 'searchindex-02dbdfd9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
