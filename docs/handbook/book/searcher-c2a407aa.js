e8bef39cc761eff4acdae1d07ec1842114696bdc
santhreal <64453045+santhreal@users.noreply.github.com>
2026-08-31 05:36:39 -0700

    docs(handbook): rebuild the book after merging main

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4c0eaf73.js',
+                path_to_root + 'searchindex-1dcd0252.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
