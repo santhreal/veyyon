829fa4ee0d456b1cb30ac0bd7a1c98756a7509ee
santhreal <64453045+santhreal@users.noreply.github.com>
2026-08-30 22:21:36 -0700

    docs: render the handbook book for the current sources

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-499800d3.js',
+                path_to_root + 'searchindex-8156068f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
