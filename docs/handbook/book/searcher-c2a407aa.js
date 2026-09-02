8edd9f1caf5e07c0fba584453a0478a09cfac7ce
santhreal <64453045+santhreal@users.noreply.github.com>
2026-09-01 19:45:15 -0700

    docs(handbook): rebuild the book for the autoswarm models section

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-16fabfb7.js',
+                path_to_root + 'searchindex-98507b9a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
