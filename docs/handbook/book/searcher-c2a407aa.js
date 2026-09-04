fd19b39b50940ced83a89009c42262ac7abf8b9a
santhreal <64453045+santhreal@users.noreply.github.com>
2026-09-03 19:41:47 -0700

    docs(handbook): rebuild the book for the aboveFooter overlay section

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1d10cb91.js',
+                path_to_root + 'searchindex-cbf2960b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
