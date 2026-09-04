c53eb5166879d17f033efdcc65f51817a1e009bd
santhreal <64453045+santhreal@users.noreply.github.com>
2026-09-03 22:50:18 -0700

    docs(autoresearch): rebuild handbook and root changelog for the resume polish

--- docs/handbook/book/searcher-c2a407aa.js
+++ docs/handbook/book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8e08f80c.js',
+                path_to_root + 'searchindex-4cbb2f53.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
